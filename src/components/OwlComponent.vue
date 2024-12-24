40650e4d54402ae0e3479e8fc5cc0042b9d5431e
DaryaA <12345@nail.com>
2024-12-24 12:53:45 +0500

    feat: создание компонента owl

---
+++ src/components/OwlComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="col-12 col-lg-3 mb-4 mb-lg-0 appeal__owl">
+
+    <div class="owl__speech">
+      <div class="owl__speech-title fs-lg-6 fw-semibold mb-2">Кем вы хотите стать?</div>
+      <div class="owl__speech-text fs-lg-6 fw-medium">
+        Пора найти себя
+        и выбрать подходящий курс :)
+        <br>
+        <div class="mt-2 owl__speech-text fs-lg-6 fw-medium">Удачи!</div>
+      </div>
+      <span class="owl__tail owl__tail_appeal"></span>
+    </div>
+
+    <img class="owl__common owl__common_tabs" src="@/assets/images/common/owl.svg" alt="owl">
+  </div>
+</template>
