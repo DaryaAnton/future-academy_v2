2499e842a672bcc4d89aed785f2d9f60a2aceeff
DaryaA <12345@nail.com>
2024-12-24 12:53:33 +0500

    feat: создание компонента hero

---
+++ src/components/HeroComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="row">
+    <div class="col col-md-6">
+      <h1 class="hero__title">Образовательная платформа</h1>
+      <h2 class="hero__title">ХОД Future Academy</h2>
+      <p class="hero__text">Актуальные знания для новичков и профессионалов</p>
+    </div>
+  </div>
+</template>
