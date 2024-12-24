3992afda0507db2a70ad342cb3008de46973b84b
DaryaA <12345@nail.com>
2024-12-24 12:52:16 +0500

    fix: добавление логики для смены темы HeaderComponent

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
   <div id="app">
+    <HeaderComponent :theme="currentTheme"/>
     <router-view/>
     <FooterComponent/>
   </div>
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue';
 import FooterComponent from '@/components/FooterComponent.vue';
 import HomeViews from '@/views/HomeViews.vue';
 
 export default {
-  components: {FooterComponent, HomeViews}
+  components: {FooterComponent, HomeViews, HeaderComponent},
+  data() {
+    return {
+      currentTheme: 'light'
+    };
+  },
+  watch: {
+    '$route'(to) {
+      if (to.name === 'courses') {
+        this.currentTheme = 'dark';
+      } else {
+        this.currentTheme = 'light';
+      }
+    }
+  }
 }
 </script>
 
